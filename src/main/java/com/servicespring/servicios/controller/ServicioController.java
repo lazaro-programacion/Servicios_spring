84ccde7203bd6f26f65ecd2ffc3ded4e97467a80
lazaro-programacion <lazaro0089@gmail.com>
2020-02-19 09:18:48 +0100

    arriba

--- src/main/java/com/servicespring/servicios/controller/ServicioController.java
+++ src/main/java/com/servicespring/servicios/controller/ServicioController.java
@@ -4,12 +4,10 @@ import com.servicespring.servicios.servicio.HelloService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * ServicioController
